a50a12d952d4d9a8bcc30a9a68ff09964c333e22
ianb33 <ianberry33@icloud.com>
2022-11-19 13:10:01 -0500

    Added Background Support
    - Created Backgrounds prefab folder
    - Added gray background prefab
    - Added Background script

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private RectTransform rectTransform;
+    void Start()
+    {
+        UpdateTransform();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateTransform()
+    {
+        rectTransform.position = new Vector3(Screen.width / Screen.width, Screen.height / Screen.height, 1);
+
+        rectTransform.localScale = new Vector3(Screen.width / 2, Screen.height / 2, 1);
+    }
+}
